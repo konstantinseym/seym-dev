951aac8383184308639e593d87bd2436a2f8e61b
konstantinseym <konstantinseym@proton.me>
2026-07-12 23:03:18 +0300

    wip: install plugins, configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1>Konstantin Seym</h1>
+      <h1>Konstantin Seym.</h1>
       <h2>This site is under development now.</h2>
     </div>
   );
